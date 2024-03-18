3ed2294fe91319f7d96623554aecba220b54497a
Reuben James <123649904+rjames567@users.noreply.github.com>
2024-03-18 10:09:06 +0000

    Remove leading and trailing whitespace

--- betterConfigurationParser.py
+++ betterConfigurationParser.py
@@ -4,9 +4,12 @@ class ConfigParser:
 
 	def read(self, filename):
 		with open(filename, 'r') as f:
-			self._heirachy =  [i.rstrip("\n").rstrip(" ") for i in f.readlines()]
+			self._heirachy =  [i.rstrip("\n") for i in f.readlines()]
 
 		self._remove_comments()
+
+	def _remove_blank_lines(self):
+		self._heirachy = [i.rstrip(" ").lstrip(" ").lstrip("\t") for i in self._heirachy if i != '']
 			
 	def _remove_comments(self):
 		for i, k in enumerate(self._heirachy):
@@ -15,6 +18,7 @@ class ConfigParser:
 				string_start, string_end = min(k.find('"'), k.find("'")), max(k.rfind('"'), k.rfind("'"))
 				if not string_start <= comment_index <= string_end:
 					self._heirachy[i] = k[:comment_index]
+		self._remove_blank_lines()
 
 configuration = ConfigParser()
 configuration.read("basic-configuration-example.conf")
