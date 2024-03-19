35e9088277e0c413c2737bc9e74a5bcd134da5bb
Reuben James <20reuben.james@utcportsmouth.org>
2024-03-19 12:13:52 +0000

    Add method to unify multiline elements

--- betterConfigurationParser.py
+++ betterConfigurationParser.py
@@ -11,13 +11,13 @@ class ConfigParser:
 
 	def read(self, filename):
 		with open(filename, 'r') as f:
-			self._heirachy =  [i.rstrip("\n") for i in f.readlines()]
+			self._heirachy = [i for i in f.readlines()]
 
 		self._remove_comments()
 		self._flatten_split_lines()
 
 	def _remove_blank_lines(self):
-		self._heirachy = [i.rstrip(" ").lstrip(" ").lstrip("\t") for i in self._heirachy if i != '']
+		self._heirachy = [i.rstrip(" ").lstrip(" ").lstrip("\t") for i in self._heirachy if i != '' and i != "\n"]
 			
 	def _remove_comments(self):
 		block_comment = False
@@ -49,21 +49,35 @@ class ConfigParser:
 	def _flatten_split_lines(self):
 		add = False
 		add_index = 0
+		multiline_string = False
 		for i, k in enumerate(self._heirachy):
 			if add:
-				self._heirachy[add_index] += k
+				temp = k.rstrip("\n")
+				if i + 1 < len(self._heirachy):
+					if self._heirachy[i + 1][:3] in self._multiline_openings.values() or not multiline_string:
+						add_string = k.rstrip("\n")
+					else:
+						add_string = k
+				else:
+					add_string = k
 				self._heirachy[i] = ""
-				if k[-1] in self._multiline_openings.values() or k[-3:] in self._multiline_openings.values():
+				if temp[-1] in self._multiline_openings.values() or temp[-3:] in self._multiline_openings.values():
 					add = False
+					multiline_string = False
+					add_string = k.rstrip("\n")
+				self._heirachy[add_index] += add_string
 			else:
 				elems = k.split("=")
-				if len(elems):
+				if len(elems) == 2:
 					var = elems[1].lstrip(" ")
 					opening = None
 					if var[0] in self._multiline_openings.keys():
 						opening = var[0]
+						self._heirachy[i] = self._heirachy[i].rstrip("\n")
 					elif var[:3] in self._multiline_openings.keys():
 						opening = var[:3]
+						multiline_string = True
+						self._heirachy[i] = self._heirachy[i].rstrip("\n")
 					if opening:
 						location = var.find(self._multiline_openings[opening])
 						if location <= 0:
