2c9779320ae01a3667bb4b2bb948f3a80715a2e3
Reuben James <20reuben.james@utcportsmouth.org>
2024-03-20 23:05:43 +0000

    Include multiline strings with line continuation in unify lines

--- betterConfigurationParser.py
+++ betterConfigurationParser.py
@@ -18,7 +18,7 @@ class ConfigParser:
         self._split_variables()
 
     def _remove_blank_lines(self):
-        self._heirachy = [i.rstrip(" ").lstrip(" ").lstrip("\t") for i in self._heirachy if i != '' and i != "\n"]
+        self._heirachy = [i.rstrip(" ") for i in self._heirachy if i != '' and i != "\n"]
 
     def _remove_comments(self):
         block_comment = False
@@ -51,39 +51,56 @@ class ConfigParser:
         add = False
         add_index = 0
         multiline_string = False
+        remove_whitespace = False
+        remove_whitespace_change = False
         for i, k in enumerate(self._heirachy):
+            temp = k.rstrip("\n")
+            if i < len(self._heirachy):
+                if (temp.rstrip("\\")[-3:] in self._multiline_openings.keys() or multiline_string) and temp[-1] == "\\":
+                    remove_whitespace = True
+                    remove_whitespace_change = True
+                    k = temp.lstrip()[:-1]
             if add:
-                temp = k.rstrip("\n")
+                if remove_whitespace and not remove_whitespace_change:
+                    k = temp.lstrip()
+                    if temp[-1] != "\\":
+                        remove_whitespace = False
+                    else:
+                        k = k[:-1]  # Otherwise removes last of the 3 quotation marks
                 if i + 1 < len(self._heirachy):
-                    if self._heirachy[i + 1][:3] in self._multiline_openings.values() or not multiline_string:
+                    if self._heirachy[i + 1][-3:] in self._multiline_openings.values() or not multiline_string:
                         add_string = k.rstrip("\n")
                     else:
                         add_string = k
                 else:
                     add_string = k
-                self._heirachy[i] = ""
                 if temp[-1] in self._multiline_openings.values() or temp[-3:] in self._multiline_openings.values():
                     add = False
                     multiline_string = False
                     add_string = k.rstrip("\n")
                 self._heirachy[add_index] += add_string
+                self._heirachy[i] = ""
             else:
                 elems = k.split("=")
                 if len(elems) == 2:
                     var = elems[1].lstrip(" ")
                     opening = None
+                    line = self._heirachy[i].rstrip("\n")
+                    if remove_whitespace and remove_whitespace_change:
+                        line = line[:-1]
                     if var[0] in self._multiline_openings.keys():
                         opening = var[0]
-                        self._heirachy[i] = self._heirachy[i].rstrip("\n")
-                    elif var[:3] in self._multiline_openings.keys():
-                        opening = var[:3]
+                        self._heirachy[i] = line
+                    elif var.rstrip("\\")[:3] in self._multiline_openings.keys():
+                        opening = var.rstrip("/")[:3]
                         multiline_string = True
-                        self._heirachy[i] = self._heirachy[i].rstrip("\n")
+                        self._heirachy[i] = line
                     if opening:
                         location = var.find(self._multiline_openings[opening])
                         if location <= 0:
                             add = True
                             add_index = i
+            remove_whitespace_change = False
         self._remove_blank_lines()
         self._remove_trailing_whitespace()
 
@@ -97,3 +114,5 @@ class ConfigParser:
 
 configuration = ConfigParser()
 configuration.read("basic-configuration-example.conf")
+
+print(configuration._heirachy)
